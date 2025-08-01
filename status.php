d03f0a493578f74e34c43e496f9e7cf86b346c7e
Philipp123623 <p.s.stueve@gmail.com>
2025-08-01 18:20:08 +0200

    Second Commit

--- status.php
+++ status.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // --------- .env laden ---------
 function loadEnv($path) {
     if (!file_exists($path)) {
