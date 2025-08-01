5a69418845b2aa3d9d52f087796b8f4558e5a785
Philipp123623 <p.s.stueve@gmail.com>
2025-08-01 14:40:54 +0200

    Second Commit

--- invite.php
+++ invite.php
@@ -1,5 +1,5 @@
 <?php
 // index.php
-header("Location: https://discord.com/oauth2/authorize?client_id=811733599509544962&permissions=1899359446&scope=bot%20applications.commands");
+header("Location: https://discord.com/oauth2/authorize?client_id=1113403511045107773&permissions=1899359446&scope=bot%20applications.commands");
 exit();
 ?>
